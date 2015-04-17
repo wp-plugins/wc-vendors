41b2d9d0bd50c988fb15a58f0aed7f3537fa65c0
digitalchild <digitalchild@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-17 00:13:50 +0000

    v1.6.2
    git-svn-id: https://plugins.svn.wordpress.org/wc-vendors/trunk@1136737 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- classes/admin/emails/class-emails.php
+++ classes/admin/emails/class-emails.php
@@ -78,10 +78,9 @@ class WCV_Emails
 	 */
 	function show_vendor_in_email( $name, $_product )
 	{
-		$product = get_post( $_product->id );
-
+		$product = get_post( $_product->id );		
 		$sold_by = WCV_Vendors::is_vendor( $product->post_author )
-			? sprintf( '<a href="%s">%s</a>', WCV_Vendors::get_vendor_shop_page( $product->post_author ), WCV_Vendors::get_vendor_shop_name( $product->post_author ) )
+			? sprintf( '<a href="%s">%s</a>', WCV_Vendors::get_vendor_shop_page( $product->post_author ), WCV_Vendors::get_vendor_sold_by( $product->post_author ) )
 			: get_bloginfo( 'name' );
 
 		$name .= '<small class="wcvendors_sold_by_in_email"><br />' . apply_filters('wcvendors_sold_by_in_email', __( 'Sold by: ', 'wcvendors' )). $sold_by . '</small><br />';
